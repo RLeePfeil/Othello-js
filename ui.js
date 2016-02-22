2d1830eea059d934f3df3487ff67be2d3b0e4f8a
Lee Pfeil <R.Lee.Pfeil@gmail.com>
2016-02-21 18:03:11 -0800

    ui.js

---
+++ ui.js
@@ -0,0 +1,18 @@
+// For the UI, menu, etc. Anything not directly related to the game.
+// Assume we have jquery.
+$.ready(function(){
+  // Set up the dials click handlers
+  $(".ui-dial").
+
+  // Set up menu
+});
+
+var OthelloUI = OthelloUI ||
+{
+  //
+  dialHandler: function(e) {
+    console.log(e.target);
+
+    //
+  }
+}

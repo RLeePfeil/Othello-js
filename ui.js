709e77d1f922487f4d81f9644a9a89267b5e2a96
Lee Pfeil <R.Lee.Pfeil@gmail.com>
2016-02-21 19:10:42 -0800

    turn on and off player 1 and player 2 ai

--- ui.js
+++ ui.js
@@ -1,6 +1,17 @@
 // For the UI, menu, etc. Anything not directly related to the game.
 // Assume we have jquery.
-$.ready(function(){
+$(document).ready(function(){
+  console.log("initialize UI");
+
+  // See which switches need to be turned off by default
+  if (!GAME.players.player1.ai) {
+    $("#control-p1").addClass("off");
+  }
+
+  if (!GAME.players.player2.ai) {
+    $("#control-p2").addClass("off");
+  }
+
   // Set up the switch click handlers
   $(".switch").on("click", OthelloUI.switchHandler)
 
@@ -10,36 +21,47 @@ $.ready(function(){
 var OthelloUI = OthelloUI ||
 {
   switchHandler: function(e) {
+    var $dt = $(e.delegateTarget);
+    var id = $dt.attr("id");
     var off = false;
 
-    switch $(e.target).attr("id") {
+    switch (id) {
       case "control-p1":
-        if (GAME.players.player1.ai) {
-          GAME.players.player1.ai = false;
+        if (playerCheck("player1")) {
           off = true;
-        } else {
-          GAME.players.player1.ai = true;
         }
         break;
       case "control-p2":
-        if (GAME.players.player2.ai) {
-          GAME.players.player2.ai = false;
+        if (playerCheck("player2")) {
           off = true;
-        } else {
-          GAME.players.player2.ai = true;
         }
         break;
       default:
         break;
     }
 
+    // Shorthand func, returns true if AI is being turned off
+    function playerCheck(which) {
+      if (GAME.players[which].ai) {
+        GAME.players[which].ai = false;
+        return true;
+      } else {
+        GAME.players[which].ai = true;
+        // If it's the computer player's turn, move!
+        if (GAME.player == which) {
+          GAME.computerMove();
+        }
+        return false;
+      }
+    }
+
     // Show the switch turning on / off
   	if (off) {
   		// Toggle off
-  		$(e.target).addClass('off');
+  		$dt.addClass('off');
   	} else {
   		// Toggle on
-  		$(e.target).removeClass('off');
+  		$dt.removeClass('off');
   	}
   },
 

604ce34c95b9cd774252240a6bc8a7ab4ffb767c
Lee Pfeil <R.Lee.Pfeil@gmail.com>
2016-02-21 18:26:56 -0800

    add switch

--- ui.js
+++ ui.js
@@ -1,18 +1,47 @@
 // For the UI, menu, etc. Anything not directly related to the game.
 // Assume we have jquery.
 $.ready(function(){
-  // Set up the dials click handlers
-  $(".ui-dial").
+  // Set up the switch click handlers
+  $(".switch").on("click", OthelloUI.switchHandler)
 
-  // Set up menu
+  // Set up menu TODO
 });
 
 var OthelloUI = OthelloUI ||
 {
-  //
-  dialHandler: function(e) {
-    console.log(e.target);
+  switchHandler: function(e) {
+    var off = false;
 
-    //
-  }
+    switch $(e.target).attr("id") {
+      case "control-p1":
+        if (GAME.players.player1.ai) {
+          GAME.players.player1.ai = false;
+          off = true;
+        } else {
+          GAME.players.player1.ai = true;
+        }
+        break;
+      case "control-p2":
+        if (GAME.players.player2.ai) {
+          GAME.players.player2.ai = false;
+          off = true;
+        } else {
+          GAME.players.player2.ai = true;
+        }
+        break;
+      default:
+        break;
+    }
+
+    // Show the switch turning on / off
+  	if (off) {
+  		// Toggle off
+  		$(e.target).addClass('off');
+  	} else {
+  		// Toggle on
+  		$(e.target).removeClass('off');
+  	}
+  },
+
+
 }
